5ed919c900ada574641b6db125d52e0b45faac7e
roslynn <39137049+roslynn@users.noreply.github.com>
2018-11-11 13:40:08 +0000

    Init with Tesseract nuget
    Add ImageProcessing Project with Tesseract nuget and equivalent Test project to play with guts
    https://github.com/charlesw/tesseract

---
+++ Tests/ImageProcessing.Tests/ImageOcrExtractorTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace ImageProcessing.Tests
+{
+  [TestClass]
+  public class ImageOcrExtractorTests
+  {
+    [TestMethod]
+    public void ProcessTest()
+    {
+      var picName = "phototest.tif";
+      var testImagePath = Path.Combine(Directory.GetCurrentDirectory(), $@"Resources\{picName}");
+      Assert.IsTrue(Directory.Exists(Path.GetDirectoryName(testImagePath)));
+
+      var processor = new ImageOcrExtractor();
+      processor.Process(picName);
+    }
+  }
+}
---
+++ WineRater/ImageProcessing/ImageOcrExtractor.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using Tesseract;
+
+/// <summary>
+/// Example taken from https://github.com/charlesw/tesseract-samples/tree/master/src/Tesseract.ConsoleDemo
+/// charlesw/tesseract-samples is licensed under the
+/// Apache License 2.0
+/// </summary>
+
+namespace ImageProcessing
+{
+  public class ImageOcrExtractor
+  {
+    private static readonly string TESDATA_NAME = "tessdata";
+
+    public ImageOcrExtractor()
+    {
+    }
+
+    public void Process(string testImagePath)
+    {
+      try
+      {
+        using (var engine = new TesseractEngine(ImageOcrExtractor.GetTestDataPath, "eng", EngineMode.Default))
+        {
+          using (Pix img = Pix.LoadFromFile(testImagePath))
+          {
+            using (var page = engine.Process(img))
+            {
+              var text = page.GetText();
+              Console.WriteLine("Mean confidence: {0}", page.GetMeanConfidence());
+
+              Console.WriteLine("Text (GetText): \r\n{0}", text);
+              Console.WriteLine("Text (iterator):");
+              using (var iter = page.GetIterator())
+              {
+                iter.Begin();
+
+                do
+                {
+                  do
+                  {
+                    do
+                    {
+                      do
+                      {
+                        if (iter.IsAtBeginningOf(PageIteratorLevel.Block))
+                        {
+                          Console.WriteLine("<BLOCK>");
+                        }
+
+                        Console.Write(iter.GetText(PageIteratorLevel.Word));
+                        Console.Write(" ");
+
+                        if (iter.IsAtFinalOf(PageIteratorLevel.TextLine, PageIteratorLevel.Word))
+                        {
+                          Console.WriteLine();
+                        }
+                      } while (iter.Next(PageIteratorLevel.TextLine, PageIteratorLevel.Word));
+
+                      if (iter.IsAtFinalOf(PageIteratorLevel.Para, PageIteratorLevel.TextLine))
+                      {
+                        Console.WriteLine();
+                      }
+                    } while (iter.Next(PageIteratorLevel.Para, PageIteratorLevel.TextLine));
+                  } while (iter.Next(PageIteratorLevel.Block, PageIteratorLevel.Para));
+                } while (iter.Next(PageIteratorLevel.Block));
+              }
+            }
+          }
+        }
+      }
+      catch (Exception e)
+      {
+        Trace.TraceError(e.ToString());
+        Console.WriteLine("Unexpected Error: " + e.Message);
+        Console.WriteLine("Details: ");
+        Console.WriteLine(e.ToString());
+      }
+    }
+
+    private static string GetTestDataPath
+    {
+      get
+      {
+        return Path.Combine(Path.GetDirectoryName(Assembly.GetAssembly(typeof(ImageOcrExtractor)).Location), TESDATA_NAME);
+      }
+    }
+  }
+}
